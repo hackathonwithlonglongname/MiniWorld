5a57e961bbbca6965aba2dff33c58d73c736fc6f
Submergence2000 <48410070+Submergence2000@users.noreply.github.com>
2019-04-14 11:08:40 +0800

    审核逻辑修改

--- pages/component/mine/user/user.js
+++ pages/component/mine/user/user.js
@@ -5,25 +5,33 @@ cloud.init()
 const db = cloud.database()
 
 Page({
-  data:{
-    thumb:'',
-    nickname:'',
-    orders:[],
-    hasinformation:false,
-    information:{},
-    is_admin:false
+  data: {
+    thumb: '',
+    nickname: '',
+    orders: [],
+    hasinformation: false,
+    information: {},
+    is_admin_list: ['oMJfy5C6sNcexW04x_8qMdk7dsdc', 'oMJfy5CqZKQWSVslILzVOZVYbLGg', 'oMJfy5F1JvBNLLZaXoPS2tTKVU5o','oMJfy5BWgD0U61BqFS6WEYGk3Pao']
   },
-  onLoad(){
-    var self = this;
+  onLoad() {
+    wx.cloud.callFunction({
+      name: 'get_id',
+      complete: res => {
+        console.log('callFunction test result: ', res)
+        this.setData({
+          openid: res.result.openid
+        })
+      }
+    })
   },
-  onShow(){
+  onShow() {
     var self = this;
     /**
      * 获取本地缓存 个人信息
      */
     wx.getStorage({
       key: 'information',
-      success: function(res){
+      success: function(res) {
         self.setData({
           hasInformation: true,
           information: res.data,
