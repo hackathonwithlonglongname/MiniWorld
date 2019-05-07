8130ec81e8efc099dd9af01bf0c12f336b07ea7f
lgt <644699280@qq.com>
2019-05-07 23:14:33 +0800

    time

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ App({
    * 
    */
   onLaunch: function () {
+    var timestamp = Date.parse(new Date())
+    var nowDate = new Date(timestamp)
+    timestamp = timestamp / 1000
+
+    //获取当前时间
+    //年
+    var Y = nowDate.getFullYear()
+    //月
+    var M = (nowDate.getMonth() + 1 < 10 ? '0' + (nowDate.getMonth() + 1) : nowDate.getMonth() + 1)
+    //日
+    var D = nowDate.getDate() < 10 ? '0' + nowDate.getDate() : nowDate.getDate()
+    this.globalData.time.year = Y
+    this.globalData.time.month = M
+    this.globalData.time.day = D
+    
     wx.cloud.callFunction({
       name: 'get_id',
       complete: res => {
@@ -31,6 +46,11 @@ App({
     openid: '',
     adminList: ['oMJfy5C6sNcexW04x_8qMdk7dsdc', 'oMJfy5CqZKQWSVslILzVOZVYbLGg', 'oMJfy5F1JvBNLLZaXoPS2tTKVU5o', 'oMJfy5BWgD0U61BqFS6WEYGk3Pao'],
     isAdmin: false,
+    time:{
+      year:'',
+      month:'',
+      day:''
+    }
   },
   showErrorModal: function (content, title) {
     wx.showModal({
