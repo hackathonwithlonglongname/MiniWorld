b9af33f94dd0e010eedfd939428eb7ee5e976a20
斯文人 <37544325+171250563@users.noreply.github.com>
2019-05-08 02:08:22 +0800

    修复了首次拒绝后后期无法调用的bug

--- pages/component/post/post.js
+++ pages/component/post/post.js
@@ -152,7 +152,7 @@ Page({
       region: e.detail.value
     })
   },
-  selectlocation: function () {
+  selectLocation: function () {
     var _this = this
     wx.chooseLocation({
       success: function (res) {
@@ -163,7 +163,53 @@ Page({
         })
       },
       fail: function () {
-        // fail
+        wx.getSetting({
+          success: function (res) {
+            var statu = res.authSetting;
+            if (!statu['scope.userLocation']) {
+              wx.showModal({
+                title: '是否授权当前位置',
+                content: '需要获取您的地理位置，请确认授权，否则地图功能将无法使用',
+                success: function (tip) {
+                  if (tip.confirm) {
+                    wx.openSetting({
+                      success: function (data) {
+                        if (data.authSetting["scope.userLocation"] === true) {
+                          wx.showToast({
+                            title: '授权成功',
+                            icon: 'success',
+                            duration: 1000
+                          })
+                          //授权成功之后，再调用chooseLocation选择地方
+                          wx.chooseLocation({
+                            success: function (res) {
+                              obj.setData({
+                                addr: res.address
+                              })
+                            },
+                          })
+                        } else {
+                          wx.showToast({
+                            title: '授权失败',
+                            icon: 'success',
+                            duration: 1000
+                          })
+                        }
+                      }
+                    })
+                  }
+                }
+              })
+            }
+          },
+          fail: function (res) {
+            wx.showToast({
+              title: '调用授权窗口失败',
+              icon: 'success',
+              duration: 1000
+            })
+          }
+        })
       },
       complete: function () {
         // complete
